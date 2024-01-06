0af27ebb20504c207cc3cfa66021279a67f601a5
Moshadi Hansamali <99788907+it21025594@users.noreply.github.com>
2024-01-07 02:11:47 +0530

    Verify the user registration process.spec.ts

--- tests/Verify the user registration process.spec.ts
+++ tests/Verify the user registration process.spec.ts
@@ -1,22 +1,25 @@
 import { test, expect, Page } from '@playwright/test';
-
+//url for the registration page
 const RegistrationURL = "https://onlinelibrary.wiley.com/action/registration?acdl-redirect=true";
 
 test ('Verify the user registration process on onlinelibrary.wiley.com', async ({ page }: { page: Page }) => {
-  
+  //navigate to the registration page
   await page.goto(RegistrationURL);
 
-  
+  //fill in the user details
   await page.fill('input[name="email"]', 'example@email.com');
   await page.fill('input[name="password"]', 'StrongPassword@123');
 
   await page.fill('input[name="firstName"]', 'Moshadi');
   await page.fill('input[name="lastName"]', 'Hansamali');
 
+  //accept the terms and conditions
   await page.check('input[name="terms"]');
 
+  //click the register button
   await page.click('button[type="Register"]');
 
+  //wait for the navigation to complete after successful registration 
   await page.waitForNavigation();
 
   expect(page.url()).toContain(200);
